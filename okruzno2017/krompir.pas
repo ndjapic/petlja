14cc43f8eb42360718d610fbf2685ff3e6a7a874
Nenad Đapić <ndjapic@users.noreply.github.com>
2017-06-25 17:13:24 +0200

    Create krompir.pas

---
+++ okruzno2017/krompir.pas
@@ -0,0 +1,26 @@
+program krompir;
+var
+ n,a,b,k: Int64;
+ m,i,x,y: integer;
+ xfree, yfree: array [1..1000000] of boolean;
+begin
+ readln(n, m);
+ for i := 1 to n do begin
+  xfree[i] := true;
+  yfree[i] := true
+ end;
+ a := n; b := n;
+ for i := 1 to m do begin
+  readln(x, y);
+  if xfree[x] then begin
+   xfree[x] := false;
+   a := a - 1
+  end;
+  if yfree[y] then begin
+   yfree[y] := false;
+   b := b - 1
+  end;
+ end;
+ k := n * n - a * b;
+ writeln(k)
+end.
